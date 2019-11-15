55194c8108185dae28872d43f523517e3b043b84
Stanislav <kinshyn.stas.ea@gmail.com>
2019-11-15 11:20:08 +0200

    Update main.js

--- clinic/js/main.js
+++ clinic/js/main.js
@@ -427,7 +427,7 @@ class Slider{
 		let x = this.box;		
 		let touchPointStart = event.changedTouches['0'].screenX;
 		let touchPointCurrent = 0;
-		this.touchBlockFlag = false;
+		//this.touchBlockFlag = false;
 
 		let touchMoveBinded = touchMove.bind(this);
 		function touchMove(event){
@@ -438,20 +438,20 @@ class Slider{
 				event.preventDefault();
 				this.slideMove({direction: 'left'});
 				touchPointStart = touchPointCurrent;
-				this.touchBlockFlag = true;
+				//this.touchBlockFlag = true;
 				touchEnd.call(this,event);
 			} else if(m <= -document.body.offsetWidth/4){
 				event.preventDefault();
 				this.slideMove({direction: 'right'});
 				touchPointStart = touchPointCurrent;
-				this.touchBlockFlag = true;
+				//this.touchBlockFlag = true;
 				touchEnd.call(this,event);				
 			}
 
   		}
 
 		function touchEnd(event){
-	    	if(!this.touchBlockFlag) return;
+	    	//if(!this.touchBlockFlag) return;
 	    	event.preventDefault();
 			this.box.removeEventListener('touchmove', touchMoveBinded);
 			let touchPointStart = 0;
