f3312e5b958a06cc62b3ad62ea5d22615a11f447
Stanislav <kinshyn.stas.ea@gmail.com>
2019-11-29 17:34:20 +0200

    canvas

--- canvas/t2/js/script.js
+++ canvas/t2/js/script.js
@@ -1,7 +1,7 @@
 let canvas = document.getElementById('c1');
 let ctx = canvas.getContext('2d');
 
-function changeCanvasSize(id){
+/*function changeCanvasSize(id){
 	let canvas = document.getElementById(id);
 	canvas.width = document.body.offsetWidth;
 	canvas.height = document.body.offsetHeight;
@@ -9,17 +9,16 @@ function changeCanvasSize(id){
 
 changeCanvasSize('c1');
 
-window.addEventListener('resize',() => changeCanvasSize('c1'));
+window.addEventListener('resize',() => changeCanvasSize('c1'));*/
 
 
 function drawContour(){
-	let w = canvas.width;
-	let h = canvas.height;
+	//let w = canvas.width;
+	//let h = canvas.height;
 
 	ctx.beginPath();
-	ctx.moveTo(w,0);
-	ctx.bezierCurveTo(w - 240, h / 2, (w - 951) / 1, h / 2, w - 951, h);
-	//ctx.quadraticCurveTo((w - 951) / 3, h / 2, w - 951, h);
+	ctx.moveTo(950,0);
+	ctx.bezierCurveTo(850,100,350,0,0,880);
 	ctx.stroke();
 }
 
