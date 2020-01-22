8365e9559d8e2b395b7def3b83a7f9be130eb368
Stanislav <kinshyn.stas.ea@gmail.com>
2020-01-22 13:43:07 +0200

    canvas

---
+++ canvas/t5/cube.js
@@ -0,0 +1,30 @@
+//Рисуем вращающийся куб
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+//
+
+var texture = new THREE.TextureLoader().load( "img/textures/leha.jpg" );
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set( 4, 4 );
+//
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, texture );
+scene.add( cube );
+
+camera.position.z = 5;
+//
+
+function render() {
+	requestAnimationFrame( render );
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+	renderer.render( scene, camera );
+}
+render();
