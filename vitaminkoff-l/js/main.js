db78a7c629d857be2fe3b3bdee948fa10b59b5c2
Stas <verstka.stanislavkinshyn@gmail.com>
2020-04-01 14:22:29 +0300

    v

--- vitaminkoff-l/js/main.js
+++ vitaminkoff-l/js/main.js
@@ -12,6 +12,11 @@ window.onload = function(){
 
 
     resizeXWrapper();
+
+
+    classMultiplyWrapper(Curier,{
+        selector: '#faceAnim',
+    });
 };
 
 
@@ -307,52 +312,60 @@ function resizeXWrapper(func,event){
 };
 
 
-window.addEventListener('DOMContentLoaded', function(){
-    document.querySelectorAll('.filter_item-date_input').forEach(input => {
-      var myDatepicker = input;
+class Curier{
+    constructor(params){
+        this.container = params.item;
+        this.cloud = this.container.querySelector('#faceCloud');
+        this.bushes = this.container.querySelector('#faceBushes');
+        this.ground = this.container.querySelector('#faceGround');
+        this.man = this.container.querySelector('#faceMan');
 
-      var daysArr = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
-      var monthArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-      var monthLongArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-      var textToday = 'Today';
-      var textClear = 'Clear';
+        this.mouseX = 1;
+        this.mouseY = 1;
+        //this.mouseEnter = this.mouseEnter.bind(this);
+        this.mouseMove = this.mouseMove.bind(this);
+        //this.mouseLeave = this.mouseLeave.bind(this);
+        document.addEventListener('mousemove', this.mouseMove);
+    }
 
-      /*if(siteLocalization){
-        if(siteLocalization == 'uk'){
-            daysArr = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-            monthArr = ['Січ', 'Лют', 'Бер', 'Квіт', 'Трав', 'Черв', 'Лип', 'Серп', 'Вер', 'Жовт', 'Лист', 'Груд'];
-            monthLongArr = ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'];
-            textToday = 'Сьогодні';
-            textClear = 'Очистити';
-        } else if(siteLocalization == 'ru'){
-            daysArr = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-            monthArr = ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июнь', 'Июль', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'];
-            monthLongArr = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-            textToday = 'Сегодня';
-            textClear = 'Очистить';
+    mouseMove(event){
+        this.screenWidth = window.innerWidth;
+        this.screenHeight = window.innerHeight;
+        let diffX = event.screenX;
+        let diffY = event.screenY;
+
+        if(diffX != this.mouseX){
+            let perX = parseInt((diffX / this.screenWidth) * 56);
+            if(perX < 0) perX = 0;
+            if(perX > 56) perX = 56;
+            let perY = 17;
+            if(perX <= 8){
+                perY = (4 / (perX + 1)) + 13;
+            } else {
+                perY = (((perX - 8) / 50) * 6) + 13;
+            }
+            this.man.style.left = `${perX}%`;
+            this.man.style.top = `${perY}%`;
+
+            let perC = parseInt((diffX / this.screenWidth) * 60) - 30;
+            if(perC < -30) perC = -30;
+            if(perC > 30) perC = 30;
+            console.log(perC)
+
+            this.cloud.style.left = `${-perC}%`;
         }
-      }*/
 
-      myDatepicker.DatePickerX.init({
-        mondayFirst      : true,
-        format           : 'dd.mm.yyyy',
-        minDate          : new Date(0, 0),
-        maxDate          : new Date(9999, 11, 31),
-        weekDayLabels    : daysArr,
-        shortMonthLabels : monthArr,
-        singleMonthLabels: monthLongArr,
-        todayButton      : true,
-        todayButtonLabel : textToday,
-        clearButton      : true,
-        clearButtonLabel : textClear,
-      });
-    });
+        if(diffY != this.mouseX){
+            let perY = parseInt((diffY / this.screenHeight) * 50) - 40;
+            if(perY < -40) perY = -40;
+            if(perY > 10) perY = 10;
 
-    document.addEventListener('change', function(event){
-        if(!event.target.closest('.filter_item-date_input')) return;
+            this.bushes.style.left = `${perY}%`;
+            //this.cloud.style.top = `${perY}%`;
+        }
 
-        let input = event.target.closest('.filter_item-date_input');
-        let span = input.closest('.filter_item-date_content').querySelector('.filter_item-date_value');
-        span.textContent = input.value;
-    })
-});
+        this.mouseX = event.screenX;
+        this.mouseY = event.screenY;
+        //console.log(event.screenX);
+    }
+};
