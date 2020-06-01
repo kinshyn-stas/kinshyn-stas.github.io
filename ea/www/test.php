fa934964a430fa36db6b613a59a48e76cd980f44
Stanislav <kinshyn.stas.ea@gmail.com>
2020-06-01 17:44:44 +0300

    ea

--- ea/www/test.php
+++ ea/www/test.php
@@ -40,113 +40,166 @@
 						</div>
 					</div>
 				</div>
-				<div class="container-fluid">
-					<div class="testimonial_box">
+			</div>
+		</section>
+		<section class="container-fluid secondblock">
+			<div class="example_box">
 
-						<div class="testimonial_item">
-							<div class="testimonial_content">
-								<div class="testimonial_text">
-									<p class="testimonial_title">Татьяна Марченко</p>
-									<p class="testimonial_undertitle">Менеджер по работе с клиентами</p>
-									<div class="testimonial_text_content">
-										<p>Имея свой сайт с 2006 года, мы обратились в агенство easy-agency с просьбой оценить наш сайт на актуальность. В результате оценки специалистами этой компании было рекомендовано разработать новый современный сайт с учетом необходимости онлайн-работы на всех видах устройствах.</p>
-										<p>Поскольку Inveria является одним из основных предприятий украинской авиационной промышленности, то и требования к сайту нашей компании максимальные. Сайт, разработанный для нас агенством easy-agency, вполне соответствует заданному уровню и выполняет все требования.</p>
-									</div>
-								</div>
-								<figure class="testimonial_media">
-									<img class="testimonial_image" src="img/mockup.png" alt="" />
-									<a class="testimonial_image_link">Смотреть оригинал</a>
-								</figure>
+				<div class="example_item">
+					<figure class="example_image">
+						<img src="img/examples/example_1.png" alt="" />
+					</figure>
+					<h3 class="example_title">Inveria</h3>
+					<p>Экосистема для интеллектуального и творческого развития</p>
+				</div>
+
+				<div class="example_item">
+					<figure class="example_image">
+						<img src="img/examples/example_2.png" alt="" />
+					</figure>
+					<h3 class="example_title">Green Tech Trade</h3>
+					<p>Официальный представитель солнечных панелей First Solar и Zorlu Energy в Украине</p>
+				</div>
+
+				<div class="example_item">
+					<figure class="example_image">
+						<img src="img/examples/example_3.png" alt="" />
+					</figure>
+					<h3 class="example_title">Наш Формат</h3>
+					<p>Издательство, специализирующееся на нехудожественной литературе (нон-фикшн)</p>
+				</div>
+
+				<div class="example_item">
+					<figure class="example_image">
+						<img src="img/examples/example_4.png" alt="" />
+					</figure>
+					<h3 class="example_title">Klitschko Expo</h3>
+					<p>Выставка-музей достижений легендарных братьев Кличко</p>
+				</div>
+
+				<div class="example_item">
+					<figure class="example_image">
+						<img src="img/examples/example_5.png" alt="" />
+					</figure>
+					<h3 class="example_title">Vera Brezhneva</h3>
+					<p>Официальный представитель солнечных панелей First Solar и Zorlu Energy в Украине</p>
+				</div>
+
+				<div class="example_item">
+					<figure class="example_image">
+						<img src="img/examples/example_6.png" alt="" />
+					</figure>
+					<h3 class="example_title">Self Invest</h3>
+					<p>Легальное трудоустройство студентов в Германии в период зимних и летних каникул</p>
+				</div>
+
+			</div>
+		</section>
+		<section class="container-fluid thirdblock">
+			<div class="testimonial_box">
+
+				<div class="testimonial_item">
+					<div class="testimonial_content">
+						<div class="testimonial_text">
+							<p class="testimonial_title">Татьяна Марченко</p>
+							<p class="testimonial_undertitle">Менеджер по работе с клиентами</p>
+							<div class="testimonial_text_content">
+								<p>Имея свой сайт с 2006 года, мы обратились в агенство easy-agency с просьбой оценить наш сайт на актуальность. В результате оценки специалистами этой компании было рекомендовано разработать новый современный сайт с учетом необходимости онлайн-работы на всех видах устройствах.</p>
+								<p>Поскольку Inveria является одним из основных предприятий украинской авиационной промышленности, то и требования к сайту нашей компании максимальные. Сайт, разработанный для нас агенством easy-agency, вполне соответствует заданному уровню и выполняет все требования.</p>
 							</div>
 						</div>
-
-						<div class="testimonial_item">
-							<div class="testimonial_content">
-								<div class="testimonial_text">
-									<p class="testimonial_title">Владислав Демченко</p>
-									<p class="testimonial_undertitle">Директор Green Tech Trade</p>
-									<div class="testimonial_text_content">
-										<p>Имея свой сайт с 2006 года, мы обратились в агенство easy-agency с просьбой оценить наш сайт на актуальность. В результате оценки специалистами этой компании было рекомендовано разработать новый современный сайт с учетом необходимости онлайн-работы на всех видах устройствах.</p>
-										<p>Поскольку Inveria является одним из основных предприятий украинской авиационной промышленности, то и требования к сайту нашей компании максимальные. Сайт, разработанный для нас агенством easy-agency, вполне соответствует заданному уровню и выполняет все требования.</p>
-									</div>
-								</div>
-								<figure class="testimonial_media">
-									<img class="testimonial_image" src="img/mockup_2.png" alt="" />
-									<a class="testimonial_image_link">Смотреть оригинал</a>
-								</figure>
-							</div>
-						</div>
-
-						<div class="testimonial_item">
-							<div class="testimonial_content">
-								<div class="testimonial_text">
-									<p class="testimonial_title">Татьяна Марченко</p>
-									<p class="testimonial_undertitle">Менеджер по работе с клиентами</p>
-									<div class="testimonial_text_content">
-										<p>Имея свой сайт с 2006 года, мы обратились в агенство easy-agency с просьбой оценить наш сайт на актуальность. В результате оценки специалистами этой компании было рекомендовано разработать новый современный сайт с учетом необходимости онлайн-работы на всех видах устройствах.</p>
-										<p>Поскольку Inveria является одним из основных предприятий украинской авиационной промышленности, то и требования к сайту нашей компании максимальные. Сайт, разработанный для нас агенством easy-agency, вполне соответствует заданному уровню и выполняет все требования.</p>
-									</div>
-								</div>
-								<figure class="testimonial_media">
-									<img class="testimonial_image" src="img/mockup.png" alt="" />
-									<a class="testimonial_image_link">Смотреть оригинал</a>
-								</figure>
-							</div>
-						</div>
-
-						<div class="testimonial_item">
-							<div class="testimonial_content">
-								<div class="testimonial_text">
-									<p class="testimonial_title">Владислав Демченко</p>
-									<p class="testimonial_undertitle">Директор Green Tech Trade</p>
-									<div class="testimonial_text_content">
-										<p>Имея свой сайт с 2006 года, мы обратились в агенство easy-agency с просьбой оценить наш сайт на актуальность. В результате оценки специалистами этой компании было рекомендовано разработать новый современный сайт с учетом необходимости онлайн-работы на всех видах устройствах.</p>
-										<p>Поскольку Inveria является одним из основных предприятий украинской авиационной промышленности, то и требования к сайту нашей компании максимальные. Сайт, разработанный для нас агенством easy-agency, вполне соответствует заданному уровню и выполняет все требования.</p>
-									</div>
-								</div>
-								<figure class="testimonial_media">
-									<img class="testimonial_image" src="img/mockup_2.png" alt="" />
-									<a class="testimonial_image_link">Смотреть оригинал</a>
-								</figure>
-							</div>
-						</div>
-
-						<div class="testimonial_item">
-							<div class="testimonial_content">
-								<div class="testimonial_text">
-									<p class="testimonial_title">Татьяна Марченко</p>
-									<p class="testimonial_undertitle">Менеджер по работе с клиентами</p>
-									<div class="testimonial_text_content">
-										<p>Имея свой сайт с 2006 года, мы обратились в агенство easy-agency с просьбой оценить наш сайт на актуальность. В результате оценки специалистами этой компании было рекомендовано разработать новый современный сайт с учетом необходимости онлайн-работы на всех видах устройствах.</p>
-										<p>Поскольку Inveria является одним из основных предприятий украинской авиационной промышленности, то и требования к сайту нашей компании максимальные. Сайт, разработанный для нас агенством easy-agency, вполне соответствует заданному уровню и выполняет все требования.</p>
-									</div>
-								</div>
-								<figure class="testimonial_media">
-									<img class="testimonial_image" src="img/mockup.png" alt="" />
-									<a class="testimonial_image_link">Смотреть оригинал</a>
-								</figure>
-							</div>
-						</div>
-
-						<div class="testimonial_item">
-							<div class="testimonial_content">
-								<div class="testimonial_text">
-									<p class="testimonial_title">Владислав Демченко</p>
-									<p class="testimonial_undertitle">Директор Green Tech Trade</p>
-									<div class="testimonial_text_content">
-										<p>Имея свой сайт с 2006 года, мы обратились в агенство easy-agency с просьбой оценить наш сайт на актуальность. В результате оценки специалистами этой компании было рекомендовано разработать новый современный сайт с учетом необходимости онлайн-работы на всех видах устройствах.</p>
-										<p>Поскольку Inveria является одним из основных предприятий украинской авиационной промышленности, то и требования к сайту нашей компании максимальные. Сайт, разработанный для нас агенством easy-agency, вполне соответствует заданному уровню и выполняет все требования.</p>
-									</div>
-								</div>
-								<figure class="testimonial_media">
-									<img class="testimonial_image" src="img/mockup_2.png" alt="" />
-									<a class="testimonial_image_link">Смотреть оригинал</a>
-								</figure>
-							</div>
-						</div>
-
+						<figure class="testimonial_media">
+							<img class="testimonial_image" src="img/mockup.png" alt="" />
+							<a class="testimonial_image_link">Смотреть оригинал</a>
+						</figure>
 					</div>
 				</div>
+
+				<div class="testimonial_item">
+					<div class="testimonial_content">
+						<div class="testimonial_text">
+							<p class="testimonial_title">Владислав Демченко</p>
+							<p class="testimonial_undertitle">Директор Green Tech Trade</p>
+							<div class="testimonial_text_content">
+								<p>Имея свой сайт с 2006 года, мы обратились в агенство easy-agency с просьбой оценить наш сайт на актуальность. В результате оценки специалистами этой компании было рекомендовано разработать новый современный сайт с учетом необходимости онлайн-работы на всех видах устройствах.</p>
+								<p>Поскольку Inveria является одним из основных предприятий украинской авиационной промышленности, то и требования к сайту нашей компании максимальные. Сайт, разработанный для нас агенством easy-agency, вполне соответствует заданному уровню и выполняет все требования.</p>
+							</div>
+						</div>
+						<figure class="testimonial_media">
+							<img class="testimonial_image" src="img/mockup_2.png" alt="" />
+							<a class="testimonial_image_link">Смотреть оригинал</a>
+						</figure>
+					</div>
+				</div>
+
+				<div class="testimonial_item">
+					<div class="testimonial_content">
+						<div class="testimonial_text">
+							<p class="testimonial_title">Татьяна Марченко</p>
+							<p class="testimonial_undertitle">Менеджер по работе с клиентами</p>
+							<div class="testimonial_text_content">
+								<p>Имея свой сайт с 2006 года, мы обратились в агенство easy-agency с просьбой оценить наш сайт на актуальность. В результате оценки специалистами этой компании было рекомендовано разработать новый современный сайт с учетом необходимости онлайн-работы на всех видах устройствах.</p>
+								<p>Поскольку Inveria является одним из основных предприятий украинской авиационной промышленности, то и требования к сайту нашей компании максимальные. Сайт, разработанный для нас агенством easy-agency, вполне соответствует заданному уровню и выполняет все требования.</p>
+							</div>
+						</div>
+						<figure class="testimonial_media">
+							<img class="testimonial_image" src="img/mockup.png" alt="" />
+							<a class="testimonial_image_link">Смотреть оригинал</a>
+						</figure>
+					</div>
+				</div>
+
+				<div class="testimonial_item">
+					<div class="testimonial_content">
+						<div class="testimonial_text">
+							<p class="testimonial_title">Владислав Демченко</p>
+							<p class="testimonial_undertitle">Директор Green Tech Trade</p>
+							<div class="testimonial_text_content">
+								<p>Имея свой сайт с 2006 года, мы обратились в агенство easy-agency с просьбой оценить наш сайт на актуальность. В результате оценки специалистами этой компании было рекомендовано разработать новый современный сайт с учетом необходимости онлайн-работы на всех видах устройствах.</p>
+								<p>Поскольку Inveria является одним из основных предприятий украинской авиационной промышленности, то и требования к сайту нашей компании максимальные. Сайт, разработанный для нас агенством easy-agency, вполне соответствует заданному уровню и выполняет все требования.</p>
+							</div>
+						</div>
+						<figure class="testimonial_media">
+							<img class="testimonial_image" src="img/mockup_2.png" alt="" />
+							<a class="testimonial_image_link">Смотреть оригинал</a>
+						</figure>
+					</div>
+				</div>
+
+				<div class="testimonial_item">
+					<div class="testimonial_content">
+						<div class="testimonial_text">
+							<p class="testimonial_title">Татьяна Марченко</p>
+							<p class="testimonial_undertitle">Менеджер по работе с клиентами</p>
+							<div class="testimonial_text_content">
+								<p>Имея свой сайт с 2006 года, мы обратились в агенство easy-agency с просьбой оценить наш сайт на актуальность. В результате оценки специалистами этой компании было рекомендовано разработать новый современный сайт с учетом необходимости онлайн-работы на всех видах устройствах.</p>
+								<p>Поскольку Inveria является одним из основных предприятий украинской авиационной промышленности, то и требования к сайту нашей компании максимальные. Сайт, разработанный для нас агенством easy-agency, вполне соответствует заданному уровню и выполняет все требования.</p>
+							</div>
+						</div>
+						<figure class="testimonial_media">
+							<img class="testimonial_image" src="img/mockup.png" alt="" />
+							<a class="testimonial_image_link">Смотреть оригинал</a>
+						</figure>
+					</div>
+				</div>
+
+				<div class="testimonial_item">
+					<div class="testimonial_content">
+						<div class="testimonial_text">
+							<p class="testimonial_title">Владислав Демченко</p>
+							<p class="testimonial_undertitle">Директор Green Tech Trade</p>
+							<div class="testimonial_text_content">
+								<p>Имея свой сайт с 2006 года, мы обратились в агенство easy-agency с просьбой оценить наш сайт на актуальность. В результате оценки специалистами этой компании было рекомендовано разработать новый современный сайт с учетом необходимости онлайн-работы на всех видах устройствах.</p>
+								<p>Поскольку Inveria является одним из основных предприятий украинской авиационной промышленности, то и требования к сайту нашей компании максимальные. Сайт, разработанный для нас агенством easy-agency, вполне соответствует заданному уровню и выполняет все требования.</p>
+							</div>
+						</div>
+						<figure class="testimonial_media">
+							<img class="testimonial_image" src="img/mockup_2.png" alt="" />
+							<a class="testimonial_image_link">Смотреть оригинал</a>
+						</figure>
+					</div>
+				</div>
+
 			</div>
    		</section>
 		<!--<section class="container-fluid secondblock">
