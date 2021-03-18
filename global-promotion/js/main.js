f8216b6b6b063f7292c277a256a554cc9297957f
kinshyn-stas <kinshyn.stas.ea@gmail.com>
2021-03-18 23:09:59 +0200

    global-promotion

---
+++ global-promotion/js/main.js
@@ -0,0 +1,82 @@
+"use strict";
+
+window.onload = function(){
+    document.addEventListener('click', clickItemHandler);
+
+    document.addEventListener('mouseover', mouseMenuHandler);
+};
+
+
+function functionMultiplyWrapper(func){
+    try{
+        func();
+    } catch(err){
+        console.log(err);
+    }
+};
+
+
+function classMultiplyWrapper(Cls,parametrs){
+    document.querySelectorAll(parametrs.selector).forEach((item) => {
+        try{
+            parametrs.item = item;
+            new Cls(parametrs);
+        } catch(err){
+            console.log(err);
+        }
+    })
+};
+
+
+function clickItemHandler(event){
+    if(!event.target.closest('.click-item')) return;
+    let item = event.target.closest('.click-item');
+    //if(item.getAttribute('href') && item.getAttribute('href') == '#') event.preventDefault();
+    let parent;
+    if(event.target.closest('.click-obj')) parent = event.target.closest('.click-obj');
+
+    let obj = {
+        'toggle': function(target){
+            parent.classList.toggle('active');
+        },
+
+        'popup-open': function(target){
+            if(item.dataset.label){
+                document.querySelector(item.dataset.label).classList.add('active');
+            } else {
+                console.log('укажите селектор требуемого элемента в data-label нажимаемой кнопки')
+            }
+        },
+
+        'menu-toggle': function(target){
+            document.querySelector('.header_block').classList.toggle('active');
+        },
+    }
+
+    if(item.dataset.action){
+        let actions = item.dataset.action.split(' ');
+        actions.forEach(action => obj[action](item));
+    } else {
+        obj['toggle'](item);
+    }
+};
+
+
+function mouseMenuHandler(event){
+    if(event.target.closest('.header_nav_list_item-trigger')){
+        document.querySelectorAll('.header_nav_list_item-trigger').forEach(item => item.classList.remove('active'))
+        event.target.closest('.header_nav_list_item-trigger').classList.add('active')
+    }
+
+    if(event.target.closest('.header_nav_list_item_list_item')){
+        let target = event.target.closest('.header_nav_list_item_list_item');
+        document.querySelectorAll('.header_nav_list_item_list_item').forEach(item => item.classList.remove('active'))
+        target.classList.add('active');
+        if(target.dataset.src && document.querySelector('.header_nav_right_img')){
+            let img = document.querySelector('.header_nav_right_img');
+            img.classList.remove('active');
+            img.src = target.dataset.src;
+            img.classList.add('active');
+        }
+    }
+};
